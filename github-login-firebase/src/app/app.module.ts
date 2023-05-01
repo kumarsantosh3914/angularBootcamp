790b64974eabe55f0aa5f8aadae2d94af6286c1e
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-05-02 01:24:11 +0530

    Added project github-login-firebase with setup done

---
+++ github-login-firebase/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ReposComponent,
+    UserCardComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    SigninComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
